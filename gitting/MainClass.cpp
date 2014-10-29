584956c0b70e8968bfcc8da29450e1876905d1b7
mhamad asadolahi <mhamad_asadolahi@yahoo.com>
2014-10-29 15:07:32 +0330

    clear update

--- gitting/MainClass.cpp
+++ gitting/MainClass.cpp
@@ -23,18 +23,6 @@ template<typename Ob,typename... Objects> void print (
 		cout <<obj << endl;
 		print<int >(objects...);
 	}
-//template<typename T>
-//void print (const T& arg)
-//	{
-//		std::cout << arg << std::endl;
-//	}
-//template<typename T, typename ... Types>
-//void print (const T& firstArg ,const Types&... args)
-//	{
-//		std::cout << firstArg << std::endl; // print first argument
-//		print(args...);
-//// call print() for remaining arguments
-//	}
 auto main ( )->int
 	{
 		print<int,int>(1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9);

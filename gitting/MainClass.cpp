22d575b53a8ed14788f6e28d324010e267c2fd82
mhamad asadolahi <mhamad_asadolahi@yahoo.com>
2014-10-29 14:38:23 +0330

    project start

---
+++ gitting/MainClass.cpp
@@ -0,0 +1,19 @@
+/*
+ * MainClass.cpp
+ *  Created on: Oct 29, 2014
+ *  Author: Mohamad Asadolahi
+ *	CrazyMachine 2014;
+ */
+
+#include "MainClass.h"
+#include <iostream>
+using namespace std;
+namespace CrazyMachine
+	{
+
+	} /* namespace CrazyMachine */
+auto main ( )->int
+	{
+		cout << endl << "program ended";
+		return 0;
+	}
---
+++ gitting/MainClass.h
@@ -0,0 +1,20 @@
+/*
+ * MainClass.h
+ *  Created on: Oct 29, 2014
+ *  Author: Mohamad Asadolahi
+ *	CrazyMachine 2014;
+ */
+
+#ifndef MAINCLASS_H_
+#define MAINCLASS_H_
+
+namespace CrazyMachine
+	{
+
+	class MainClass
+		{
+		};
+
+	} /* namespace CrazyMachine */
+
+#endif /* MAINCLASS_H_ */

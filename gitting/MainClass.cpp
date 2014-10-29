4d351d9a248473e1cc5a7a69248b8ee8e1b91571
mhamad asadolahi <mhamad_asadolahi@yahoo.com>
2014-10-29 15:04:26 +0330

    second update

--- gitting/MainClass.cpp
+++ gitting/MainClass.cpp
@@ -12,8 +12,32 @@ namespace CrazyMachine
 	{
 
 	} /* namespace CrazyMachine */
+
+template<typename Ob> void print (const Ob& object)
+	{
+		cout << object << endl;
+	}
+template<typename Ob,typename... Objects> void print (
+		const Ob& obj ,const Objects&... objects)
+	{
+		cout <<obj << endl;
+		print<int >(objects...);
+	}
+//template<typename T>
+//void print (const T& arg)
+//	{
+//		std::cout << arg << std::endl;
+//	}
+//template<typename T, typename ... Types>
+//void print (const T& firstArg ,const Types&... args)
+//	{
+//		std::cout << firstArg << std::endl; // print first argument
+//		print(args...);
+//// call print() for remaining arguments
+//	}
 auto main ( )->int
 	{
+		print<int,int>(1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9);
 		cout << endl << "program ended";
 		return 0;
 	}
